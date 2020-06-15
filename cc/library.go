5f381832c1672a8b9ab1f6833568277e6707efab
Jingwen Chen <jingwen@google.com>
2020-06-15 05:24:19 +0000

    Support multiple dists per Android.bp module, and dist output selection.
    This CL adds "dists" to the base property struct to support multiple
    dist file configurations, and generic tag support to dist tagged outputs
    of modules.
    
    Fixes: b/152834186
    Test: soong tests and `m sdk dist`
    
    Change-Id: I80c86bc9b7b09e671f640a4480c45d438bdd9a2a
    Signed-off-by: Jingwen Chen <jingwen@google.com>
    Signed-off-by: Akash Srivastava <akashniki@gmail.com>

--- cc/library.go
+++ cc/library.go
@@ -372,7 +372,7 @@ type libraryDecorator struct {
 	unstrippedOutputFile android.Path
 
 	// Location of the file that should be copied to dist dir when requested
-	distFile android.OptionalPath
+	distFile android.Path
 
 	versionScriptPath android.ModuleGenPath
 
@@ -907,7 +907,7 @@ func (library *libraryDecorator) linkStatic(ctx ModuleContext,
 			library.injectVersionSymbol(ctx, outputFile, versionedOutputFile)
 		} else {
 			versionedOutputFile := android.PathForModuleOut(ctx, "versioned", fileName)
-			library.distFile = android.OptionalPathForPath(versionedOutputFile)
+			library.distFile = versionedOutputFile
 			library.injectVersionSymbol(ctx, outputFile, versionedOutputFile)
 		}
 	}
@@ -1001,11 +1001,11 @@ func (library *libraryDecorator) linkShared(ctx ModuleContext,
 			library.injectVersionSymbol(ctx, outputFile, versionedOutputFile)
 		} else {
 			versionedOutputFile := android.PathForModuleOut(ctx, "versioned", fileName)
-			library.distFile = android.OptionalPathForPath(versionedOutputFile)
+			library.distFile = versionedOutputFile
 
 			if library.stripper.needsStrip(ctx) {
 				out := android.PathForModuleOut(ctx, "versioned-stripped", fileName)
-				library.distFile = android.OptionalPathForPath(out)
+				library.distFile = out
 				library.stripper.stripExecutableOrSharedLib(ctx, versionedOutputFile, out, builderFlags)
 			}
 
